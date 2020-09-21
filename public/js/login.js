48845bedeaef19b6d7dbc5c008c422c276b933de
noxiousHellcat <68965448+noxiousHellcat@users.noreply.github.com>
2020-09-21 19:36:05 +0800

    I have made a poor choice on the font but have gone too far to change it

--- public/js/login.js
+++ public/js/login.js
@@ -1,30 +1,22 @@
 // Get the modal
-var reg_modal = document.getElementById("reg_Modal");
-var for_modal = document.getElementById("forgot_Modal");
+var reg_modal = document.getElementById("register");
+var for_modal = document.getElementById("forgot");
 
 var regbtn = document.getElementById("reg_mdl");
 var forgbtn = document.getElementById("for_mdl");
 
-var span_reg = document.getElementsByClassName("close1")[0];
-var span_for = document.getElementsByClassName("close2")[0];
-
-
 // When the user clicks the button, open the modal 
 regbtn.onclick = function() {
   reg_modal.style.display = "block";
+  reg_modal.style.animation = "fadeIn 1s";
+  for_modal.style.display = "none";
+  
 }
 
 forgbtn.onclick = function() {
-    for_modal.style.display = "block";
-  }
-
-// When the user clicks on <span> (x), close the modal
-span_reg.onclick = function() {
-  reg_modal.style.display = "none";
-}
-
-span_for.onclick = function() {
-    for_modal.style.display = "none";
+    for_modal.style.display = "block"
+    for_modal.style.animation = "fadeIn 1s";
+    reg_modal.style.display = "none";
   }
 
 // When the user clicks anywhere outside of the modal, close it
